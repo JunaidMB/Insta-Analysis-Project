fe530387099d9a45462ad843f38dd60f1674fa93
Raghad <72172564+RaghadZur@users.noreply.github.com>
2021-06-27 19:39:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import io
 
 st.title('Raghads Attempt at Instagram Dashboard')
 
-
 username = 'raghad.artistry'
 
 js = json.load(open('C:/Users/ragha/raghad.artistry/raghad.artistry.json', encoding='utf-8'))
@@ -35,10 +34,40 @@ with open(filename, "wb") as f:
     f.write(response.content)
 
 col1, col2 = st.beta_columns(2)
-col1.image(filename)
+col1.image('C:/Users/ragha/raghad.artistry/image.jpg')
 col2.write(f"Biography: {js['GraphProfileInfo']['info']['biography']}")
 col2.write(f"Is Business Account: {js['GraphProfileInfo']['info']['is_business_account']}")
 col2.write(f"Number Of Posts: {js['GraphProfileInfo']['info']['posts_count']}")
-         
-for n in js['GraphProfileInfo']["info"]["followers_count"]:
-    print(n)
+
+      
+#number of likes
+df['likes']=df['edge_media_preview_like'].apply(lambda x: x['count'])
+#number of comments
+df['comments']=df['edge_media_to_comment'].apply(lambda x: x['count'])
+#transform the timestamp to datetime object
+df['date']=df['taken_at_timestamp'].apply(datetime.fromtimestamp)
+#extract dayofweek, month, week, year, ym 
+df['dayofweek']=df['date'].dt.dayofweek
+df['month']=df['date'].dt.month
+df['week']=df['date'].dt.week
+df['year']=df['date'].dt.year
+df['ym']=df['year'].astype(str)+df['month'].astype(str)
+engagement_rate=(((df['likes'].sum()+df['comments'].sum())/len(df))/js['GraphProfileInfo']['info']['followers_count'])*100
+col2.write(f"Average Posts Per Month: {round(df.groupby('ym').size().mean(),2)}")
+col2.write(f"Engagement Rate: {round(engagement_rate,2)}%")
+
+x=df.groupby('dayofweek').size()
+st.subheader('Number Of Posts Per Week-Day')
+st.bar_chart(pd.DataFrame(x).rename(columns={0: 'Number Of Posts'}))
+x=df.groupby('month').size()
+st.subheader('Number Of Posts Per Month')
+st.bar_chart(pd.DataFrame(x).rename(columns={0: 'Number Of Posts'}))
+
+#for n in js['GraphProfileInfo']["info"]["followers_count"]:
+ #   print(n)
+ #
+
+df.shape
+
+list(df)
+df.head(3)
